8394571e3c811baef16e6210e6e4f87e8e137e2d
ana-GT <ahuaman3@gatech.edu>
2013-10-29 14:48:15 -0400

    Added Taxonomy stuff

---
+++ ui/Taxonomy/taxonomyDlg.h
@@ -0,0 +1,42 @@
+//######################################################################
+//
+// Author : A. Huaman Quispe
+// $Id: 
+//
+//######################################################################
+
+/*! \file
+  \brief Implements the PlannerDlg, the grasp planner dialog box. 
+*/
+
+//Added by qt3to4:
+#include <QTextStream>
+#include <QDialog>
+#include <QFile>
+#include "ui_taxonomyDlg.h"
+
+
+/*! \class PlannerDlg
+  \brief Creates and controls the grasp planner dialog box.
+*/
+
+class TaxonomyDlg : public QDialog, public Ui::TaxonomyDlgUI
+{
+    Q_OBJECT
+	private:
+    QTextStream stream;
+    QFile masterFile;
+    
+    void init();
+    void destroy() ;
+ public:
+ TaxonomyDlg(QWidget *parent = 0) : QDialog(parent) {
+	setupUi(this);
+	init();
+    }
+    ~TaxonomyDlg(){destroy();}
+    public slots:
+
+    void readGraspFile();
+    
+};
